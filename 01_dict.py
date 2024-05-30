9441caeaff0ff4aee5329d2b1b41c313ad08b4a1
Shantanu Biswas <155043274+shantanucoder@users.noreply.github.com>
2024-05-30 07:11:41 +0530

    Add files via upload

---
+++ 01_dict.py
@@ -0,0 +1,12 @@
+oxford = {
+    "gift": "Something willingly given to someone to appreciate", 
+    "this": "A keyword in c++",
+    "Youtube": "A video sharing platform",
+    "Instagram": "A picture taking and storing platform",
+    "myList": [1, 3, 45],
+    }
+
+# print(oxford)
+print(oxford['Youtube'])
+print(oxford.get('youtube')) #keys are the case sensitive parts of the program
+
---
+++ 03_sets.py
@@ -0,0 +1,14 @@
+mySet = {1, 34, 53} #here 1 is acting as an integer  
+mySet.add(45)
+mySet.add("1") #here 1 is acting as a string  
+print(mySet)
+print(" The length of set is: ", len(mySet))
+
+
+mySet.remove(34)
+mySet.add("1")
+print(mySet.pop())
+
+print(mySet)
+print(" The length of set is: ", len(mySet))
+
